9821d6e60d0a2ce21f8eca44ef2f768471c99f15
vjunfel <vjunfel@gmail.com>
2024-11-21 23:17:00 +0800

    added content to hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "@/components/Hero";
 
 function Home() {
 	return (
-		<main className="font-[family-name:var(--font-poppins)] min-h-screen bg-white text-gray-900">
+		<main className="font-[family-name:var(--font-poppins)] min-h-screen bg-gray-100 text-gray-900 xl:px-40 md:px-10 px-5">
       <Hero />
 		</main>
 	);
