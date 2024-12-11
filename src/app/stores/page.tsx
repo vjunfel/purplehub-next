49b257f926b68f184f0aa5bc40dbe1e04aec562a
vjunfel <vjunfel@gmail.com>
2024-12-12 01:37:06 +0800

    Added time on store info

--- src/app/stores/page.tsx
+++ src/app/stores/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import store from '../../../public/images/store.jpg'
 import Image from 'next/image'
 import { storeData } from '@/constants'
-import { MapPin, Phone } from 'lucide-react'
+import { MapPin, Phone, Timer } from 'lucide-react'
 
 const StoresLocation: React.FC = () => {
 	return (
@@ -10,10 +10,10 @@ const StoresLocation: React.FC = () => {
 			<div className='pt-[160px] w-full max-w-screen-xl mx-auto min-h-screen flex justify-center items-center flex-col '>
 				<h2 className="my-10 text-5xl ">Our Stores</h2>
 				
-				<div className='flex flex-col md:flex-row gap-16  w-full h-full py-10 md:px-16 rounded-3xl items-start'>
+				<div className='flex flex-col md:flex-row gap-16 w-full h-full py-10 lg:pb-32 md:px-16 rounded-3xl items-start'>
 					<div className="flex flex-col flex-1 gap-10">
 						{storeData.map((item, index) => (
-							<div key={index} className='flex flex-col gap-4 border border-purple-400 p-5 rounded-lg bg-purple-900'>
+							<div key={index} className='flex flex-col gap-4 border border-purple-400 p-5 lg:p-10 rounded-lg bg-white text-gray-700'>
 								<div className='flex items-center mb-2'>
 									<p className="text-3xl lg:text-4xl font-medium">{item.name}</p>
 									<sub className='ml-2 text-sm'> - {item.branch}</sub>
@@ -29,6 +29,12 @@ const StoresLocation: React.FC = () => {
 										<MapPin className='w-6 h-6'/>
 									</span>
 									<p className='ml-2'>{item.address}</p>
+								</div>
+								<div className='flex h-auto'>
+									<span>
+										<Timer className='w-6 h-6'/>
+									</span>
+									<p className='ml-2'>{item.time}</p>
 								</div>
 							</div>
 						))}
